c294fa0d70535ffcd38ff0c53ab1ed8366268b32
Tims999 <75502646+Tims999@users.noreply.github.com>
2020-12-04 22:17:45 -0300

    git commit -m "Hello World"
    git push

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
 def main():
-  # print("Keep it logically awesome.")
+#  print "Keep it logically awesome."
+   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+#  f = open("quotes.txt")
+#  quotes = f.readlines()
+#  f.close()
+  
+#  print quotes
+  
 
 if __name__== "__main__":
   main()
